16699cfe1eabf10091a2f84b41fd05b07057f03a
Josh <josh@joshthomas.dev>
2024-05-13 11:41:51 -0500

    adjust tests

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -9,10 +9,14 @@ from django.test import override_settings
 from django_opfield.conf import app_settings
 
 
+@patch.dict(os.environ, {"OP_CLI_PATH": ""})
 class TestOPCliPath:
-    @override_settings(DJANGO_OPFIELD={"OP_CLI_PATH": None})
-    def test_default(self):
-        assert app_settings.OP_CLI_PATH is None
+    @patch("shutil.which")
+    def test_default(self, mock_which):
+        mock_which.return_value = None
+
+        with pytest.raises(ImportError):
+            assert app_settings.OP_CLI_PATH
 
     @override_settings(DJANGO_OPFIELD={"OP_CLI_PATH": "path/to/op"})
     def test_user_setting(self):
@@ -23,16 +27,7 @@ class TestOPCliPath:
         assert "path/to/op" in str(app_settings.OP_CLI_PATH)
 
     @patch("shutil.which")
-    @patch.dict(os.environ, {"OP_CLI_PATH": None})
     def test_shutil_which(self, mock_which):
         mock_which.return_value = "path/to/op"
 
         assert "path/to/op" in str(app_settings.OP_CLI_PATH)
-
-    @patch("shutil.which")
-    @patch.dict(os.environ, {"OP_CLI_PATH": None})
-    def test_get_op_cli_path_none(self, mock_which):
-        mock_which.return_value = None
-
-        with pytest.raises(ImportError):
-            assert app_settings.OP_CLI_PATH
